a2a88df5ecef92c6010b3f748077719512421493
Madannadar <154582270+Madannadar@users.noreply.github.com>
2024-09-23 15:34:07 +0000

    arrowfunction

--- 03_Basics/ArrowFunction.js
+++ 03_Basics/ArrowFunction.js
@@ -1,4 +1,4 @@
-// watch it from start again
+// ------------------------------------this------------------------------------------
 const user = {
     username: "Madan",
     price: 100,
@@ -8,6 +8,47 @@ const user = {
     }
 
 }
-user.welcomeMessage()
-user.username = "Arun"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "Arun"
+// user.welcomeMessage()
+
+console.log(this); // output {}
+
+
+// function chai(){
+//     let username = "Arun"
+//     console.log(this.username);     // this cant be used in functions 
+// }
+
+// chai()
+
+
+// const chai = function(){  // both are same
+//     let username = "Arun"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = () => {  // this is not used here to here means arrow functions 
+//     let username = "Madan"
+//     console.log(this.username);
+// }
+// chai()
+
+
+//---------------------------Arrow function-------------------------------------
+
+// const addTwo = (n1,n2) =>{
+//     return n1 + n2
+// }
+
+// const addTwo = (n1,n2) => n1 + n2 // if only one statement then can be done like this 
+// // implisite return, it understands by it self ke return karna h 
+
+// console.log(addTwo(3,4));
+
+
+// const name = (num1,num2) => ({username:"madan"})
+// console.log(name(3,4));
+
