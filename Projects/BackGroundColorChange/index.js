bc93877c48ba295b34e6a2ec9c68c2e73e87ca1d
Madannadar <154582270+Madannadar@users.noreply.github.com>
2024-09-30 22:07:20 +0530

    1st Project

---
+++ Projects/BackGroundColorChange/index.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach((button) => {
+  console.log(button);
+  button.addEventListener('click', function(e){ // e event object 
+    console.log(e)
+    console.log(e.target)
+    // change background color
+    switch (e.target.id){ 
+      case ('grey'):
+        body.style.backgroundColor = e.target.id;
+      case ('white'):
+        body.style.backgroundColor = e.target.id;
+      case ('blue'):
+        body.style.backgroundColor = e.target.id;
+      case ('yellow'):
+        body.style.backgroundColor = e.target.id;
+      case ('cyan'):
+        body.style.backgroundColor = e.target.id;
+    }
+  })
+})
